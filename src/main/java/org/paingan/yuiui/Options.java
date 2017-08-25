ee43370b1a2415e41e01013e9eae9253a927f325
Paulus Yansen <paulus.yansen@gmail.com>
2017-08-25 10:56:42 +0700

    first commit

---
+++ src/main/java/org/paingan/yuiui/Options.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.paingan.yuiui;
+
+/**
+ *
+ * @author paulusyansen
+ */
+public class Options {
+    public String charset = "UTF-8";
+    public int lineBreakPos = -1;
+    public boolean munge = true;
+    public boolean verbose = false;
+    public boolean preserveAllSemiColons = false;
+    public boolean disableOptimizations = false;
+}
---
+++ src/main/java/org/paingan/yuiui/YUICompressor.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.paingan.yuiui;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.Writer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+ 
+import org.apache.commons.io.IOUtils;
+import org.mozilla.javascript.ErrorReporter;
+import org.mozilla.javascript.EvaluatorException;
+ 
+import com.yahoo.platform.yui.compressor.JavaScriptCompressor;
+
+/**
+ *
+ * @author paulusyansen
+ */
+public class YUICompressor {
+    public static void main(String args[]) {
+        try {
+ 
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, e.getMessage(), e);
+        }
+    }
+ 
+    private static Logger logger = Logger.getLogger(YUICompressor.class.getName());
+    
+    public static void compressJavaScript(String inputFilename, String outputFilename, Options o) throws IOException {
+    Reader in = null;
+    Writer out = null;
+    try {
+        in = new InputStreamReader(new FileInputStream(inputFilename), o.charset);
+ 
+        JavaScriptCompressor compressor = new JavaScriptCompressor(in, new YuiCompressorErrorReporter());
+        in.close(); in = null;
+ 
+        out = new OutputStreamWriter(new FileOutputStream(outputFilename), o.charset);
+        compressor.compress(out, o.lineBreakPos, o.munge, o.verbose, o.preserveAllSemiColons, o.disableOptimizations);
+    } finally {
+        IOUtils.closeQuietly(in);
+        IOUtils.closeQuietly(out);
+    }
+}
+}
+
+class YuiCompressorErrorReporter implements ErrorReporter {
+    private static Logger logger = Logger.getLogger(YuiCompressorErrorReporter.class.getName());
+    public void warning(String message, String sourceName, int line, String lineSource, int lineOffset) {
+        if (line < 0) {
+            logger.log(Level.WARNING, message);
+        } else {
+            logger.log(Level.WARNING, line + ':' + lineOffset + ':' + message);
+        }
+    }
+ 
+    public void error(String message, String sourceName, int line, String lineSource, int lineOffset) {
+        if (line < 0) {
+            logger.log(Level.SEVERE, message);
+        } else {
+            logger.log(Level.SEVERE, line + ':' + lineOffset + ':' + message);
+        }
+    }
+ 
+    public EvaluatorException runtimeError(String message, String sourceName, int line, String lineSource, int lineOffset) {
+        error(message, sourceName, line, lineSource, lineOffset);
+        return new EvaluatorException(message);
+    }
+}
